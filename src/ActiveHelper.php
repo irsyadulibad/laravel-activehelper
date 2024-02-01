e5bf294beba0be87b6ae0015a5e9e9220847e155
sahrullahh <49519441+sahrullahh@users.noreply.github.com>
2024-02-01 16:34:28 +0700

    master: add for hidden value active pages

--- src/ActiveHelper.php
+++ src/ActiveHelper.php
@@ -6,15 +6,15 @@ use Illuminate\Support\Facades\Route;
 
 class ActiveHelper
 {
-    public static function echo(string $params, string $value = 'active'): string
+    public static function echo(string $params, string $active_value_class = 'active', string $hidden_value = ''): string
     {
         $route = Route::current()->getName();
         $params = explode(',', str_replace("'", '', $params));
-        $match = array_filter($params, function($param) use($route) {
+        $match = array_filter($params, function ($param) use ($route) {
             $param = trim($param);
-            return preg_match("/$param/", $route);
+            return preg_match("/\b$param/", $route);
         });
 
-        return (count($match) > 0) ? $value : '';
+        return (count($match) > 0) ? $active_value_class : $hidden_value;
     }
 }
